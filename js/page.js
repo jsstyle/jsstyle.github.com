8a51e3b26b5b4f3b3e953f8dd95322c4e95582cc
Ondrej Zara <ondras@zarovi.cz>
2012-10-16 18:36:38 +0200

    decoding

--- js/page.js
+++ js/page.js
@@ -7,22 +7,18 @@ var Page = {
 		this._dom.create = document.querySelector("#create");
 		this._dom.decode = document.querySelector("#decode");
 		this._dom.result = document.querySelector("#result");
+		this._dom.decodeArea = document.querySelector("#decode textarea");
 		this._dom.createButton = document.querySelector("#create button");
+		this._dom.decodeButton = document.querySelector("#decode button");
 		this._dom.resultHeading = document.querySelector("#result h2");
 		
+		this._dom.createButton.addEventListener("click", this);
+		this._dom.decodeButton.addEventListener("click", this);
+
 		this._jsstyle.build(this._dom.create);
-		
 		this._dom.create.appendChild(this._dom.createButton);
 		
-		this._dom.createButton.addEventListener("click", this);
-		
-		if (location.search.length > 1) {
-			var hash = location.search.substring(1);
-			this._jsstyle.fromHash(hash);
-			this._create();
-		} else if (!location.hash) {
-			location.hash = "about";
-		}
+		this._load();
 	},
 
 	handleEvent: function(e) {
@@ -30,19 +26,80 @@ var Page = {
 			case this._dom.createButton:
 				this._create();
 			break;
+
+			case this._dom.decodeButton:
+				this._decode();
+			break;
 		}
 		
+	},
+
+	/*
+	search \ hash | neni  | result | neco |
+	      ano     | draw  |  draw  |  -   |
+	      ne      | about |    -   |  -   |
+	*/
+	_load: function() {
+		var create = false;
+		
+		if (location.search.length > 1) { /* pre-fill with data from url */
+			var hash = location.search.substring(1);
+			this._jsstyle.fromHash(hash);
+			/* auto-create */
+			if (!location.hash || location.hash == "#result") { this._create(); }
+		} else if (!location.hash) {
+			location.hash = "about";
+		}
+	},
+	
+	_decode: function() {
+		var value = this._dom.decodeArea.value.trim();
+		if (!value) {
+			alert("Give me some data and try again...");
+			return;
+		}
+		
+		/* is it a JSON? */
+		try {
+			var data = JSON.parse(value);
+			this._jsstyle.fromJSON(data);
+			this._create();
+			return;
+		} catch (e) {};
+
+		/* is it an url? */
+		var r = value.match(/^(http.*\?)?([x0-9]+)(#.*)?$/);
+		if (r) {
+			this._jsstyle.fromHash(r[2]);
+			this._create();
+			return;
+		}
+		
+		/* is it a signature? */
+		var r = value.match(/^\+-+\+\n[\s\S]+\+-+\+$/);
+		if (r) {
+			this._jsstyle.fromAA(value);
+			this._create();
+			return;
+		}
+		
+		alert("Unable to decode pasted data :-(");
 	},
 	
 	_create: function() {
 		this._dom.result.innerHTML = "";
 		this._dom.result.appendChild(this._dom.resultHeading);
 		
-		var canvas = this._jsstyle.toCanvas();
-		this._buildResultItem("Image", "Right-click to save", canvas);
+		var json = this._jsstyle.toJSON();
+		var pre = document.createElement("pre");
+		pre.innerHTML = JSON.stringify(json, null, "  ");
+		this._buildResultItem("JSON", "Pure JSONified essence", pre);
 
 		var aa = this._jsstyle.toAA();
 		this._buildResultItem("Signature", "Hardcore ASCII art awesomeness. Select all, copy, paste!", aa);
+
+		var canvas = this._jsstyle.toCanvas();
+		this._buildResultItem("Image", "Right-click to save", canvas);
 
 		var url = "?" + this._jsstyle.toHash();
 		var link = document.createElement("a");
@@ -51,11 +108,6 @@ var Page = {
 		link.innerHTML = "Click here";
 		var aa = this._jsstyle.toAA();
 		this._buildResultItem("Permalink", "Link to this page", link);
-
-		var json = this._jsstyle.toJSON();
-		var pre = document.createElement("pre");
-		pre.innerHTML = JSON.stringify(json, null, "  ");
-		this._buildResultItem("JSON", "Pure JSONified essence", pre);
 
 		location.hash = "result";
 	},
